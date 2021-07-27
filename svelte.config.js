780c20d9fd0f69f1b1f5f6bd4caedb5e4af5753a
nikolai <nikolai@theministry.co>
2021-07-27 11:42:37 +0200

    lets try netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
+		adapter: adapter(),
 		files: {
 			assets: 'public',
 		}
