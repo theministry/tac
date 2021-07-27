7d1b2dd1760f2e44c8261f0c3441584a8e1c1bc5
nikolai <nikolai@theministry.co>
2021-07-27 14:41:36 +0200

    better imaging

--- src/lib/elements/Info.svelte
+++ src/lib/elements/Info.svelte
@@ -19,7 +19,7 @@
   <div class="images">
     <div class="inner info img">
       {#each images as image}
-        <img src="/img/{image}" alt="{image}" />
+        <img src="/img/info/{image}" alt="{image}" />
       {/each}
     </div>
   </div>
