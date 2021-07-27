f029b2afb42db71a14fb3de4f8aed83f8f243f11
nikolai <nikolai@theministry.co>
2021-07-27 05:17:30 +0200

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
