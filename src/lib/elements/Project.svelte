1650ad1f6bd10d046ef011688295cd9631e5945b
Nikolai <nikolai@theministry.co>
2021-08-09 16:56:43 +0200

    Update Project.svelte

--- src/lib/elements/Project.svelte
+++ src/lib/elements/Project.svelte
@@ -16,10 +16,10 @@
   
   const dispatch = createEventDispatcher();
 
-  const { name, action, images, author } = project.frontmatter
-  const location = getLocation(project.frontmatter.location)
-  const id = ("000" + project.frontmatter.id).slice(-3)
-  const content = marked( project.content )
+  $: { name, action, images, author } = project.frontmatter
+  $: location = getLocation(project.frontmatter.location)
+  $: id = ("000" + project.frontmatter.id).slice(-3)
+  $: content = marked( project.content )
 
   const close = () => {
     dispatch('close')
